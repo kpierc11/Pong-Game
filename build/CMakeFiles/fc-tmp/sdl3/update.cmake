a3822fec2f616150c0ffa3adab5b56e10091cf77
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2025-08-02 13:19:59 -0400

    Remove build directory from tracking

--- build/CMakeFiles/fc-tmp/sdl3/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for sdl3")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/Kaleb/Documents/GitHub/Pong-Game/build/CMakeFiles/fc-tmp/sdl3/sdl3-gitupdate.cmake")
-
-endblock()
